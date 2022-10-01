f2e0a5f47fcf95406c5dc9bfcf2cb808bb9886dc
Jonathan Krisna <jonathankrisna@protonmail.com>
2022-10-01 20:34:46 +0700

    Testing

--- src/project_pbol_2020130017/FXMLDocumentController.java
+++ src/project_pbol_2020130017/FXMLDocumentController.java
@@ -23,7 +23,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("Fuck You");
-        label.setText("Nigga~");
+        label.setText("Nigga~~~");
     }
     
     @Override
